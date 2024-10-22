325949bb840990768e7d7be32391279a3a04e594
Baptiste Zorzi <baptiste@mbz-dev.com>
2024-10-22 13:35:54 +0200

    better support

--- lua/smallest_range.lua
+++ lua/smallest_range.lua
@@ -16,6 +16,122 @@ local function calculateDelta(pos1, pos2)
     return delta
 end
 
+local function escapeForRx(str)
+    return vim.fn.escape(str, '^$.*~[]')
+end
+
+local function charUnderCursor()
+    local line = vim.fn.line(".")
+    local col = vim.fn.col(".")
+
+    local current_line_content = vim.fn.getline(".")
+    local char_under_cursor = current_line_content:sub(col, col)
+
+    return char_under_cursor
+end
+
+local function findClosestBrace(target, other, direction)
+    -- saving char position
+    local initial_cursor_pos = { vim.fn.line('.'), vim.fn.col('.') }
+
+    local stack = 1
+    local target_pos = { 0, 0 }
+
+    local flags = direction == 1 and "W" or "bW"
+
+    local rx = escapeForRx(target) .. '\\|' .. escapeForRx(other)
+    while vim.fn.search(rx, flags) > 0 do
+        local char_under_cursor = charUnderCursor()
+
+        if char_under_cursor == target then
+            stack = stack - 1
+        elseif char_under_cursor == other then
+            stack = stack + 1
+        end
+        if stack == 0 then
+            target_pos = { vim.fn.line("."), vim.fn.col(".") }
+            break
+        end
+    end
+
+    -- Restauring cursor position
+    vim.fn.cursor(initial_cursor_pos)
+
+    return target_pos
+end
+
+local function searchPairPosForIdenticalPair(char, multiline)
+    local current_line = vim.fn.line('.')
+
+    if multiline == false then
+        -- one line, We count the opening and closing chars from the start of the line
+        local line_content = vim.fn.getline(".")
+        local last_open_col = 0
+        local cursor_col = vim.fn.col(".")
+
+        -- Iterating on the character in the line
+        for col = 1, vim.fn.strlen(line_content) do
+            local char = line_content:sub(col, col)
+            if char == pair[1] then
+                if last_open_col == 0 then
+                    -- We found an opening char
+                    if col > cursor_col then
+                        -- The opening char is after the cursor, useless to continue
+                        break
+                    end
+                    last_open_col = col
+                else
+                    -- We found a closing char
+                    if col >= cursor_col then
+                        -- The cursor is before the closing, we found our pair
+                        return { { current_line, last_open_col }, { current_line, col } }
+                    else
+                        -- The closing char is before the cursor, we keep looking
+                        last_open_col = 0
+                    end
+                end
+            end
+        end
+
+        return {{0,0}, {0,0}}
+    else
+        -- multiline, things get more complicated
+        -- Not yet supported
+        return {{0,0}, {0,0}}
+    end
+end
+
+
+local function searchPairPos(pair)
+    -- redefining the searchPairPos function from vim, because the initial
+    -- function gives unconsistent results
+
+    if pair[1] == pair[2] then
+        return searchPairPosForIdenticalPair(pair[1], pair[3])
+    end
+
+    local char_under_cursor = charUnderCursor()
+
+    local pos_before = {0,0}
+    local pos_after = {0,0}
+
+    if char_under_cursor == pair[1] then
+        -- Cursor is on the opening char
+        pos_before = { vim.fn.line("."), vim.fn.col(".") }
+        pos_after = findClosestBrace(pair[2], pair[1], 1)
+    elseif char_under_cursor == pair[2] then
+        -- Cursor is on the closing char
+        pos_after = { vim.fn.line("."), vim.fn.col(".") }
+        pos_before = findClosestBrace(pair[1], pair[2], -1)
+    else
+        -- Cursor is somewhere in between
+        pos_before = findClosestBrace(pair[1], pair[2], -1)
+        pos_after = findClosestBrace(pair[2], pair[1], 1)
+    end
+
+    return { pos_before, pos_after }
+end
+
 local function findSmallestPair(pairs)
     local current_lnum = vim.fn.line(".") or 1
 
@@ -39,14 +155,9 @@ local function findSmallestPair(pairs)
         local closest_before
         local closest_after
 
-        if p[1] == p[2] then
-            -- opening and closing chars are the same
-            closest_before = vim.fn.searchpos(p[1], "cbnW")
-            closest_after = vim.fn.searchpos(p[2], "cnW")
-        else
-            closest_before = vim.fn.searchpairpos(p[1], "", p[2], "cbnW")
-            closest_after = vim.fn.searchpairpos(p[1], "", p[2], "cnW")
-        end
+        local closests = searchPairPos(p)
+        closest_before = closests[1]
+        closest_after = closests[2]
 
         local can_be_multiline = p[3]
 
@@ -68,47 +179,49 @@ end
 local function select_smallest_range(include_chars)
     local pair = findSmallestPair({
         -- { "opening", "closing" , multiline },
+        { "[", "]", true },
         { "{", "}", true },
         { "(", ")", true },
-        { "[", "]", true },
         { "<", ">", true },
         { "'", "'", false },
         { '"', '"', false },
     })
 
-    if #pair > 0 then
-        if not include_chars then
-            -- removing the chars from the pair
-
-            local eol_start = vim.fn.col({ pair[1][1], '$' })
-
-            if pair[1][2] == eol_start - 1 then
-                -- opening char is at the end of the line
-                -- adding a space after the char
-                local line = vim.fn.getline(pair[1][1])
-                vim.fn.setline(pair[1][1], line .. " ")
-            end
-            pair[1][2] = pair[1][2] + 1
-
-
-            if pair[2][2] == 1 then
-                -- closing char is at the beginning of the line
-                pair[2][1] = pair[2][1] - 1
-                pair[2][2] = vim.fn.col({ pair[2][1], '$' })
-            else
-                pair[2][2] = pair[2][2] - 1
-            end
-        end
-
-        -- setting cursor pos to opening char
-        vim.fn.cursor(pair[1])
-
-        -- activating visual mode
-        vim.cmd("normal! v")
-
-        -- setting cursor pos to closing char
-        vim.fn.cursor(pair[2])
+    if #pair == 0 then
+        return
     end
+
+    if not include_chars then
+        -- removing the chars from the pair
+
+        local eol_start = vim.fn.col({ pair[1][1], '$' })
+
+        if pair[1][2] == eol_start - 1 then
+            -- opening char is at the end of the line
+            -- adding a space after the char
+            local line = vim.fn.getline(pair[1][1])
+            vim.fn.setline(pair[1][1], line .. " ")
+        end
+        pair[1][2] = pair[1][2] + 1
+
+
+        if pair[2][2] == 1 then
+            -- closing char is at the beginning of the line
+            pair[2][1] = pair[2][1] - 1
+            pair[2][2] = vim.fn.col({ pair[2][1], '$' })
+        else
+            pair[2][2] = pair[2][2] - 1
+        end
+    end
+
+    -- setting cursor pos to opening char
+    vim.fn.cursor(pair[1])
+
+    -- activating visual mode
+    vim.cmd("normal! v")
+
+    -- setting cursor pos to closing char
+    vim.fn.cursor(pair[2])
 end
 
 return {
